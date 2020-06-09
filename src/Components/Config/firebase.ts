c50d903293ded900290663f5f90db4b983a3b4c8
Harsha Srikara <harshasrikara@gmail.com>
2020-06-08 19:11:41 -0500

    updated test suite to use .env configuration

--- src/Components/Config/firebase.ts
+++ src/Components/Config/firebase.ts
@@ -1,27 +1,27 @@
 import * as firebase from "firebase";
-import 'firebase/analytics';
+import "firebase/analytics";
 import { FirebaseConfig } from "./interface";
 
 const dev_firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.DEV_API_KEY,
-  authDomain: process.env.DEV_AUTH_DOMAIN,
-  databaseURL: process.env.DEV_DATABASAE_URL,
-  projectId: process.env.DEV_PROJECT_ID,
-  storageBucket: process.env.DEV_STORAGE_BUCKET,
-  messagingSenderId: process.env.DEV_MESSENGER_SENDER_ID,
-  appId: process.env.DEV_APP_ID,
-  measurementId: process.env.DEV_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSENGER_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID,
+  measurementId: process.env.REACT_APP_DEV_MEASUREMENT_ID,
 };
 
 const prod_firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.PROD_API_KEY,
-  authDomain: process.env.PROD_AUTH_DOMAIN,
-  databaseURL: process.env.PROD_DATABASAE_URL,
-  projectId: process.env.PROD_PROJECT_ID,
-  storageBucket: process.env.PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.PROD_MESSENGER_SENDER_ID,
-  appId: process.env.PROD_APP_ID,
-  measurementId: process.env.PROD_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSENGER_SENDER_ID,
+  appId: process.env.REACT_APP_PROD_APP_ID,
+  measurementId: process.env.REACT_APP_PROD_MEASUREMENT_ID,
 };
 
 let firebaseConfig: FirebaseConfig;
